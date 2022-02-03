aefcc1dc303958f330d14814df4620158b50a7db
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-02-03 21:39:01 +0700

    chore: switch to plugin-react (#31)

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  esbuild: {
-    jsxInject: `import React from 'react'`
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       components: resolve(__dirname, "src/components"),
       styles: resolve(__dirname, "src/styles"),
-      routes: resolve(__dirname, "src/routes")
-    }
+      routes: resolve(__dirname, "src/routes"),
+    },
   },
-})
+});
