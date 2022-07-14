744f540a0d85e46eca9412a29dc71d948e2bd85f
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-07-14 22:12:50 +0700

    chore: update packages to latest - vite 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "classic",
+    }),
+  ],
   resolve: {
     alias: {
       lib: resolve(__dirname, "src/lib"),
