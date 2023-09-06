f663b6b47429b80e2c26fe5f711185de22e538db
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-09-06 14:38:53 +0700

    chore: setup vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({ typescript: true, eslint: { lintCommand: "eslint src" } }),
+  ],
   resolve: {
     alias: [{ find: "~", replacement: resolve(__dirname, "src") }],
   },
