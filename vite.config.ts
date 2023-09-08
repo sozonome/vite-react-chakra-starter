51886d7bf9d17e69455cb1250afedb1612251410
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-09-08 17:07:53 +0700

    chore: setup million

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from "@vitejs/plugin-react";
+import million from "million/compiler";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
@@ -7,6 +8,7 @@ import checker from "vite-plugin-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    million.vite({ auto: true }),
     react(),
     checker({ typescript: true, eslint: { lintCommand: "eslint src" } }),
   ],
