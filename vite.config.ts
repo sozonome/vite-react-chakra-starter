5c18376b3d8464a45c1069d8a3e7447d46c624a4
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-11-06 12:31:14 +0700

    chore: update checker lint command

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@
 import react from '@vitejs/plugin-react';
 import million from 'million/compiler';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     million.vite({ auto: true }),
     react(),
-    checker({ typescript: true, eslint: { lintCommand: 'eslint src' } }),
+    checker({
+      typescript: true,
+      eslint: { lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"' },
+    }),
     tsconfigPaths(),
   ],
   server: {
